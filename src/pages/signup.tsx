ed698ae5e24d7e9bccabd48ba7d22a0e2d9fe290
Milena Dakesian <miledake@gmail.com>
2022-11-03 21:58:40 -0300

    signup

---
+++ src/pages/signup.tsx
@@ -0,0 +1,84 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup'
+import { Input } from '../components/Form/Input'
+import { HTTPHandler } from '../services/api'
+import { useRouter } from 'next/router'
+
+type SignUpFormData = {
+  email: string
+  password: string
+  firstName: string
+  lastName: string
+}
+
+const signUpFormSchema = yup.object({
+  email: yup.string().required('Email is required').email('Invalid email'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 characters')
+    .max(36, 'Password must be at most 36 characters'),
+  firstName: yup.string().required('First Name is required'),
+  lastName: yup.string().required('Last Name is required')
+})
+
+export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(signUpFormSchema)
+  })
+  const router = useRouter()
+
+  const handleSignUp: SubmitHandler<SignUpFormData> = async (values) => {
+    await HTTPHandler.post('/users', {
+      ...values
+    })
+      .then(() => {
+        router.push('dashboard')
+      })
+      .catch(() => {})
+  }
+
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center" bg="grain">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="white"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+        onSubmit={handleSubmit(handleSignUp)}
+        boxShadow="xl"
+        rounded="md"
+      >
+        <Stack spacing="4">
+          <Input type="email" label="Email" error={errors.email} {...register('email')} />
+          <Input
+            type="password"
+            label="Password"
+            error={errors.password}
+            {...register('password')}
+          />
+          <Input
+            type="text"
+            label="First Name"
+            error={errors.firstName}
+            {...register('firstName')}
+          />
+          <Input type="text" label="Last Name" error={errors.lastName} {...register('lastName')} />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="oxblood">
+          Sign Up
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
