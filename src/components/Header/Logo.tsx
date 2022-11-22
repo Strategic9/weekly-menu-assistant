061b32404f8d57dd3445fc332e8bc6926227edca
Carlos Navarro <carlosarturo28@gmail.com>
2022-11-22 09:51:40 +0100

    test(unit): Bugfix/fixing unit testing
    * merging develop into bugfix/fixing-unit-testing
    
    * test(header): fix the unit test for the header component

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,7 @@ import { Image, Container } from '@chakra-ui/react'
 
 export function Logo() {
   return (
-    <Container ml="0" w="250px" me="0" mr="26px" color="white">
+    <Container className="header-logo" ml="0" w="250px" me="0" mr="26px" color="white">
       <Image src="/assets/logo.svg" alt="Forkify Logo" />
     </Container>
   )
