f4e96f79e5b43da2821272d494b9be338582cdc7
Hjertelundh <119856599+Hjertelundh@users.noreply.github.com>
2023-05-23 11:12:06 +0200

    menu history dishes in chronolgical order

--- src/pages/menu/history.tsx
+++ src/pages/menu/history.tsx
@@ -41,13 +41,16 @@ export default function Menu() {
     sortedMenusHistory.filter((date) => {
       return new Date(date.startDate) < new Date()
     })
-  menusHistory.map((menu) => {
-    const dishesInMenuHistory = menu.dishes
-    dishesInMenuHistory.sort(
-      (a, b) => new Date(a.selectionDate).getTime() - new Date(b.selectionDate).getTime()
-    )
-    return dishesInMenuHistory
-  })
+
+  menusHistory &&
+    menusHistory.map((menu) => {
+      const dishesInMenuHistory = menu.dishes
+      dishesInMenuHistory &&
+        dishesInMenuHistory.sort(
+          (a, b) => new Date(a.selectionDate).getTime() - new Date(b.selectionDate).getTime()
+        )
+      return dishesInMenuHistory
+    })
 
   return (
     <PageWrapper>
